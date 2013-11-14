a9e2a6ec6405606b6c7fed98f78b8b1eead783c2
Marika Uunonen <marika@joiner.fi>
2013-11-14 15:13:11 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marika::Application.config.secret_token = '474919f74190eb4d2c6b2b45f0ccfa707bd12583b3824c6fdf62ea54165bd0a6c85950387751a4ffbff7522ecea31250a340e9a72fbb402ae4f8eda78be5611e'
